5d2e887f8794e01b6b77f4a54bf2a72735974382
Kasper Mróz <kasper.mroz@arteia.xyz>
2020-08-06 14:26:43 +0200

    minor refactoring

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,21 @@ import { initStore, logger } from 'utils';
 
 const { STORE_PATH } = Deno.env.toObject();
 
-await initStore(STORE_PATH);
+initStore(STORE_PATH);
+
+const counter = await Store.open<Counter>({
+    filePath: `${STORE_PATH}/counter.json`,
+    default: {
+        value: 0,
+    }
+});
 
 const app = new App();
 
 app.use(logger);
 
 app.get('/', async (_req: Request, res: Response) => {
-    const counter = await Store.open<Counter>({
-        filePath: `${STORE_PATH}/counter.json`,
-        default: {
-            value: 0,
-        }
-    });
-
     await counter.write((c: Counter) => c.value++);
-
     await counter.read((c: Counter) => {
         res.status(200).send(`${c.value} reuqest${c.value > 1 ? 's' : ''} so far!`);
     })
